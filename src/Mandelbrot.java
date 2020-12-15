3d25b52a2fa42a7a9360656882ce59eeacd10bca
LouisVdwl <louis.vandewalle98@gmail.com>
2020-12-15 16:11:37 +0100

    ALGO 1ère fractale avec bug

--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -7,21 +7,23 @@ public class Mandelbrot {
         Color blanc;
 
         noir = new Color(255, 0, 120);
-        blanc = new Color(0,0,0);
+        blanc = new Color(255,255,255);
         zn = new Complex(-2.5, 1.75);
 
         ImagePanel img = new ImagePanel(800, 800);
-        int red = 255;int green = 0;int blue = 120;
-        img.getImage().setRGB(20, 50, noir.getRGB());
 
         Window wnd = new Window(img, -2.5, 1.75, 1, -1.75);
 
-        for(int y = 0; y < wnd.height(); y++){
-            for(int x = 0; x<wnd.width(); x++){
+        for(int y = 0; y < 800; y++){
+            for(int x = 0; x < 800; x++){
                 Point pActuel = new Point(x, y);
-                zn = zn.square().add(wnd.toComplex(pActuel));
-                if(zn.square() < 4){
-
+                Complex c = wnd.toComplex(pActuel);
+                zn = zn.square().add(c);
+                if(zn.modulus2() < 4){
+                    System.out.println("ok");
+                    img.getImage().setRGB(x, y, noir.getRGB());
+                }else{
+                    img.getImage().setRGB(x, y, blanc.getRGB());
                 }
 
             }
